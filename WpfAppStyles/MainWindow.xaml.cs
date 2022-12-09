7097cbbaed25e2f3f9fdf3070955b67c9291c0c0
dovgoluckij_i@itstep.academy <dovgoluckij_i@itstep.academy>
2022-12-09 21:37:56 +0200

    lesson 4

---
+++ WpfAppStyles/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppStyles
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // create brush
+            LinearGradientBrush gradientBrush = new LinearGradientBrush();
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.Yellow, 0));
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.Navy, 1));
+
+            // add to rosource dictionary
+            this.Resources.Add("gradientBrush", gradientBrush);
+
+            // use static resource
+            //btnOk.Background = (Brush)this.Resources["gradientBrush"];
+            //btnOk.Background = (Brush)this.TryFindResource("gradientBrush");
+
+            // use dynamic resource
+            btnOk.SetResourceReference(BackgroundProperty, "gradientBrush");
+            btnCancel.SetResourceReference(BackgroundProperty, "gradientBrush");
+        }
+    }
+}
---
+++ WpfAppStylesDynamic/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppStylesDynamic
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            List<string> styles = new List<string>();
+            styles.Add("light");
+            styles.Add("dark");
+
+            InitializeComponent();
+            cmbSwitchTheme.SelectionChanged += CmbSwitchTheme_SelectionChanged;
+            cmbSwitchTheme.ItemsSource = styles;
+            cmbSwitchTheme.SelectedItem = "dark";
+        }
+
+        private void CmbSwitchTheme_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string style = cmbSwitchTheme.SelectedItem.ToString();
+            Uri uri = new Uri(style + ".xaml", UriKind.Relative);
+            // load resources
+            ResourceDictionary dictionary = Application.LoadComponent(uri) as ResourceDictionary;
+            // clear old resources
+            Application.Current.Resources.Clear();
+            // apply new resources
+            Application.Current.Resources.MergedDictionaries.Add(dictionary);
+        }
+    }
+}
