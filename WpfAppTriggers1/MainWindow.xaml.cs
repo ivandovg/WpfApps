2efdc96c33a9a0051a15368d33989125da4acfc7
dovgoluckij_i@itstep.academy <dovgoluckij_i@itstep.academy>
2022-12-12 21:23:26 +0200

    Lesson Triggers

---
+++ WpfAppTriggers1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppTriggers1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button == null)
+                return;
+
+            string tag = button.Tag.ToString();
+
+            switch (tag)
+            {
+                case "ok":
+                    edText.Text += "press Ok" + Environment.NewLine;
+                    break;
+                case "cancel":
+                    edText.Text += "press cancel" + Environment.NewLine;
+                    break;
+                default:
+                    edText.Text += "unknown button";
+                    break;
+            }
+        }
+    }
+}
