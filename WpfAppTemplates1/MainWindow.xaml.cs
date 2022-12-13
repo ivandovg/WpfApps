5d7a68b40221c52755c0f01021a5d908a0841386
dovgoluckij_i@itstep.academy <dovgoluckij_i@itstep.academy>
2022-12-13 21:16:35 +0200

    Lesson Templates

---
+++ WpfAppTemplates1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppTemplates1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<ToDoClass> listToDo;
+        public MainWindow()
+        {
+            listToDo = new ObservableCollection<ToDoClass>();
+            MakeTemplatesData();
+            InitializeComponent();
+
+            lstToDo.ItemsSource = listToDo;
+        }
+
+        private void MakeTemplatesData()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                listToDo.Add(
+                    new ToDoClass()
+                    {
+                        Id = i,
+                        Title = "Task " + (i + 1),
+                        Description = $"Make templates in WPF ({i})",
+                        Deadline = DateTime.Now
+                    });
+            }
+        }
+    }
+}
---
+++ WpfAppTemplates1/ToDoClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppTemplates1
+{
+    internal class ToDoClass
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Deadline { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title}, {Deadline.ToShortDateString()} {Deadline.ToShortTimeString()}";
+        }
+    }
+}
